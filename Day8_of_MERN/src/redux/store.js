b6688047f0108c56e278a1d91297aae09e1e5c67
Dilkhush saini <sainidilkhush019@gmail.com>
2025-06-09 13:08:20 +0530

    new features

--- Day8_of_MERN/src/redux/store.js
+++ Day8_of_MERN/src/redux/store.js
@@ -1,8 +1,8 @@
-import {configureStore} from "@reduxjs/toolkit"
-import counterReducer from "./counterSlice.jsx"
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice.jsx";
 
 export const store = configureStore({
-    reducer:{
-        counter: counterReducer
-    }
-})
+  reducer: {
+    counter: counterReducer,
+  },
+});
---
+++ Day9_of_MERN/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  
+  return (
+    <div>
+      <h2>App.js</h2>
+      <button>Increment</button>
+      <p>count:</p>
+    </div>
+  )
+}
+
+export default App
