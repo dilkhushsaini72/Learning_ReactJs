d1c2bf448633fd4a2a037c679471ab375ce769b9
Dilkhush saini <sainidilkhush019@gmail.com>
2025-06-12 21:08:09 +0530

    adding features in weather app

---
+++ Day11_of_MERN/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
